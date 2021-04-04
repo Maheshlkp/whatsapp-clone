63cfd2f3bfffcc51fdb5b28f0cbf9adfc748d3ce
Mahesh <Mahesh.lkp1234@gmail.com>
2021-04-04 13:02:25 -0500

    sidebar abd homepage changes

---
+++ components/Sidebar.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import styled from 'styled-components';
+import {Avatar, IconButton} from "@material-ui/core"
+import ChatIcon from '@material-ui/icons/Chat';
+// import MoreVertIcon from "@meterial-ui/icons/MoreVertIcon"
+
+function Sidebar () {
+    return (
+        <Container>
+            <Header>
+                <UserAvatar />
+                <IconsContainer>
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                        {/* <MoreVertIcon /> */}
+                    </IconButton>
+                </IconsContainer>
+            </Header>
+        </Container>
+    )
+}
+
+export default Sidebar;
+
+const Container = styled.div``;
+
+const Header = styled.div``;
+
+const UserAvatar = styled(Avatar)``;
+
+const IconsContainer = styled.div``;
